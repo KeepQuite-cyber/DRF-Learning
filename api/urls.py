45c1d10c5653fa973e7433d3fae76312aae9a50d
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-07-01 10:22:19 +0530

    get() , put() , delete() , get_object() completed

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path("students/" , views.studentView),
     path("students/<int:pk>/" , views.studentDetailView),
 
-    path("employees/", views.Employees.as_view())
+    path("employees/", views.Employees.as_view()),
+    path("employees/<int:pk>/", views.EmployeesDetailView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -60,3 +60,27 @@ class Employees(APIView):
             return Response(serializer.data , status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+
+class EmployeesDetailView(APIView):
+    def get_object(self , pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self, request , pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+
+    def put(self , request , pk):
+        serializer = EmployeeSerializer(self.get_object(pk), data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self , request , pk):
+        self.get_object(pk).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
